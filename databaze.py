5a7268c12bbe06ae094fead75d056de3ccb28bc2
matyasdufek-cell <matyas.dufek@gymkren.cz>
2025-11-26 14:44:18 +0100

    ppln

--- databaze.py
+++ databaze.py
@@ -2,14 +2,15 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="seminar.krenka.cz",
+  host=r"seminar.krenka.cz",
   user="c1seminar",
   password="ERwf6Li_6rT",
   database="c1seminar"
 )
 
 mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM pampalini")
+mycursor.execute("SELECT * FROM `pampalini`")
 myresult = mycursor.fetchall()
 for x in myresult:
   print(x)
+print(myresult)
