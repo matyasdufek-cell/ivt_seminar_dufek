cc9d9b9ee9a580a98288b7bc0a645329059ff9af
matyasdufek-cell <matyas.dufek@gymkren.cz>
2025-12-04 21:40:45 +0100

    oop bankovni ucet

--- boj.py
+++ boj.py
@@ -1,49 +1,27 @@
-"""
-lives1 = 100
-attack1 = 12
-defence1 = 5
-
-lives2 = 100
-attack2 = 10
-defence2 = 5
-
-while(lives1>0 and lives2>0):
-"""
-
-class Fighter:
-
-    def __init__(self, healthpoints, attack, defence, name):
-        self.healthpoints = healthpoints
-        self.attack = attack
-        self.defence = defence
-        self.name = name
+class BankovniUcet:
     
-    def give_report(self):
-        return f"{self.name} has {self.healthpoints} healthpoints left."
+    def __init__(self, vlastnik : str):
+        self._vlastnik = vlastnik
+        self._zustatek = 0
     
-    def defend_yourself(self, opponents_attack):
-        return opponents_attack - self.defence
+    def zjisti_zustatek(self):
+        print("Zjištění zůstatku.")
+        return self._zustatek
     
-    def commence_attack(self, opponent):
-        opponent.defend_yourself(self.attack)
-
-fighter1 = Fighter(100, 12, 5, "Pampalini")
-fighter2 = Fighter(100, 10, 5, "Kuba Kubikula")
-round = 1
-
-while(fighter1.healthpoints>0 and fighter2.healthpoints>0):
-    fighter1.commence_attack("fighter2")
-    fighter2.give_report()
-    fighter2.commence_attack("fighter1")
-    fighter1.give_report()
-
-
-"""
-while(fighter1.healthpoints>0 and fighter2.healthpoints>0):
-    fighter1.healthpoints -= (fighter2.attack - fighter1.defence)
-    fighter2.healthpoints -= (fighter1.attack - fighter2.defence)
-    print("round", round)
-    print(f"healthpoints of first fighter: {fighter1.healthpoints}")
-    print(f"healthpoints of second fighter: {fighter2.healthpoints}")
-    round += 1
-"""
+    def vloz(self, hodnota):
+        self._zustatek += hodnota
+        print(f"Vloženo {hodnota}, na účtě máte {self._zustatek}.")
+    
+    def vyber(self, hodnota):
+        if self._zustatek < hodnota:
+            print("Výběr peněz nemožný, nedostatek peněz na účtu.")
+            return
+        self._zustatek -= hodnota
+        print(f"Vybírám {hodnota}, na účtě máte {self._zustatek}.")
+        
+muj_ucet = BankovniUcet("Barack")
+muj_ucet.vloz(800)
+muj_ucet.zjisti_zustatek()
+muj_ucet.vyber(700)
+muj_ucet.vyber(700)
+print(muj_ucet._vlastnik)
